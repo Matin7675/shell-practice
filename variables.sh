99cd286cfefdb35e5e032335a3319e981d4a2e71
Matin7675 <pabdulmatin76@gmail.com>
2025-12-19 00:53:21 +0530

    Variables

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hi! How are you?"
+echo "$PERSON2: I’m fine, thank you. And you?"
+echo "$PERSON1: I’m good. What are you doing?"
+echo "$PERSON2: I’m reading a book."
+echo "$PERSON1: That sounds nice. Enjoy!"
+echo "$PERSON2: Thanks!" 
