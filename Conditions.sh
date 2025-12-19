ec39bb09f02e671a32a5e2909cd85f6c30416469
Matin7675 <pabdulmatin76@gmail.com>
2025-12-19 15:58:02 +0530

    Conditions

--- Conditions.sh
+++ Conditions.sh
@@ -3,8 +3,7 @@
 Number=20
 
 if [ $Number -lt 30 ] then
-    echo "given number is less than 30"
+    echo "given number $Number is less than 30"
 else
-    echo "given number is greater than 30"
+    echo "given number $Number is greater than 30"
 fi 
-        
